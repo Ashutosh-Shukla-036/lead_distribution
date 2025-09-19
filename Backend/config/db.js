dd6d16ec737525cf6f4394d130cb14c910dd8a1c
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2025-09-20 00:28:49 +0530

    updated backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/machine_test";
+
 
 const connectDB = async () => {
   try {
-      await mongoose.connect(process.env.MONGODB_URI, {
-    });
+    await mongoose.connect(MONGO_URL);
     console.log("MongoDB Connected");
   } catch (err) {
-      console.error("MongoDB connection failed:", err.message);
-      process.exit(1);
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
   }
 };
 
--- Backend/seedAdmin.js
+++ Backend/seedAdmin.js
@@ -4,12 +4,12 @@ import Admin from "./models/Admin.js";
 
 dotenv.config();
 
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/machine_test";
+const MONGO_URL =
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/machine_test";
 
 async function createAdmin() {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URL);
     console.log("✅ Connected to MongoDB");
 
     const email = "admin@example.com";
