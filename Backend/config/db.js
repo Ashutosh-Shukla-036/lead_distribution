531e91c7787a9a90f7ecf6285b7f7360f4d854cd
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2025-09-20 00:18:48 +0530

    Updated DB connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+      await mongoose.connect(process.env.MONGODB_URI, {
+    });
     console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+  } catch (err) {
+      console.error("MongoDB connection failed:", err.message);
+      process.exit(1);
   }
 };
 
